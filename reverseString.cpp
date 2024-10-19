79f8bc3605074cf6fde0672550242131fe28cf3f
Ramlakhan patel <138600057+ram19966p@users.noreply.github.com>
2024-10-19 22:25:29 +0530

    "Reverse _ String "

---
+++ reverseString.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+#include<string>
+using namespace std ;
+int main()
+{  
+string  str = "Ram lakhan." ;
+
+reverse(str.begin(),str.end());
+cout<< str<<endl;
+
+
+  return 0 ;
+}
+
